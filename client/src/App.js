714f5d65fb62bea03fd6b2233cab5a780b548b09
dnovakovic15 <dnovakovic21@yahoo.com>
2018-06-29 17:19:35 -0500

    Home Page React Component Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './Pages/Home'
+// import Mentor from './Pages/Mentor'
+// import Student from './Pages/Student'
 
 class App extends Component {
   render() {
@@ -10,6 +13,8 @@ class App extends Component {
         <div>
           <Switch>
             <Route exact path="/Home" component={Home}/>
+            {/*<Route exact path="/Mentor" component={Mentor}/>
+            <Route exact path="/Student" component={Student}/>*/}
           </Switch>
         </div>
       </Router>
---
+++ client/src/Pages/Home/Home.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import './Home.css';
+
+class Home extends Component{
+    render(){
+        return(
+            <div>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="navbar-brand" id="navbrand">
+                        <img src="img/logo-2.png" alt="logo" class=""/>
+                        <a class="navtext navbar-link" href="#">About The Kids</a>
+                    </div>
+
+                    <div class="navbar-collapse collapse" id="navbarResponsive">
+                        <ul class="navbar-nav ml-auto justify-content-end">
+                            <li class="nav-item px-2">
+                                <a class="navtext navbar-link" href="pages/teacherstudent/teacherstudent.html">Create Account</a>
+                            </li>
+                            <li class="nav-item px-2">
+                                <a class="navtext navbar-link" href="pages/teacherstudent/teacherstudent.html">Sign In</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
+                <div class="jumbotron jumbotron-fluid">
+                    <div class="container my-auto">
+                        <div class="row">
+                            <div class="col-lg-7 mx-auto pt-5">
+                                <br/>
+                                <br/>
+                                <div class="row">
+                                    <p class="title-text text-uppercase"><strong>About</strong></p>
+                                </div>
+                                <div class="row">
+                                    <p class="title-text text-uppercase">The Kids</p>
+                                </div>
+                                
+                            </div>
+                            <div id="cal-col" class="col-lg-5">
+                                <h1><strong>Events</strong></h1>
+                                <img id="calendar" src="img/2018-calendar.jpg" alt="Interactive Calendar"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container">
+                    <div class="row quote">
+                        <h1>"A mentor is someone who allows you to see the hope inside yourself."</h1>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
