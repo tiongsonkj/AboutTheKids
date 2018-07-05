dc1621ec2849d4a779fe423284a3fff4ea6ace40
Kelvin Tiongson <kelvintiongson30@gmail.com>
2018-07-05 09:23:33 -0500

    confirm password was not receiving input, but now is fixed

--- client/src/Pages/MentorForm/MentorForm.js
+++ client/src/Pages/MentorForm/MentorForm.js
@@ -11,7 +11,7 @@ class MentorForm extends Component {
             lastName: '',
             email: '',
             password: '',
-            confirmPassword: ''
+            password2: ''
         }
 
         this.onSubmit = this.onSubmit.bind(this);
@@ -44,6 +44,8 @@ class MentorForm extends Component {
         this.setState({
             fields
         })
+
+        console.log(fields);
     }
 
     render() {
@@ -54,23 +56,56 @@ class MentorForm extends Component {
             <form action="POST">
             <div className="form-group col-md-6 mx-auto">
                 <label htmlFor="firstName">First Name</label>
-                <input id="firstName" type="firstName" className="form-control" placeholder="Enter first name" onChange = {this.onChange} value = {this.state.firstName}/>
+                <input 
+                    id="firstName" 
+                    type="firstName" 
+                    className="form-control" 
+                    placeholder="Enter first name" 
+                    onChange = {this.onChange} 
+                    value = {this.state.firstName}
+                />
             </div>
             <div className="form-group col-md-6 mx-auto">
                 <label htmlFor="lastName">Last Name</label>
-                <input id="lastName" type="lastName" className="form-control" placeholder="Enter last name" onChange = {this.onChange} value = {this.state.lastName}/>
+                <input 
+                    id="lastName" 
+                    type="lastName" 
+                    className="form-control" 
+                    placeholder="Enter last name" 
+                    onChange = {this.onChange} 
+                    value = {this.state.lastName}
+                />
             </div>
             <div className="form-group col-md-6 mx-auto">
                 <label htmlFor="email">Email</label>
-                <input id="email" type="email" className="form-control" placeholder="john@1234.com" onChange = {this.onChange} value = {this.state.email}/>
+                <input 
+                    id="email" 
+                    type="email" 
+                    className="form-control" 
+                    placeholder="john@1234.com" 
+                    onChange = {this.onChange} 
+                    value = {this.state.email}
+                />
             </div>
             <div className="form-group col-md-6 mx-auto">
                 <label htmlFor="password">Password</label>
-                <input id="password" type="password" className="form-control" onChange = {this.onChange} value = {this.state.password}/>
+                <input 
+                    id="password" 
+                    type="password" 
+                    className="form-control" 
+                    onChange = {this.onChange} 
+                    value = {this.state.password}
+                />
             </div>
             <div className="form-group col-md-6 mx-auto">
                 <label htmlFor="password2">Confirm Password</label>
-                <input id="password2" type="confirmPassword" className="form-control" onChange = {this.onChange} value = {this.state.confirmPassword}/>
+                <input 
+                    id="password2" 
+                    type="password" 
+                    className="form-control" 
+                    onChange = {this.onChange} 
+                    value = {this.state.confirmPassword}
+                />
             </div>
             
             <button id="makementor" type="button" className="btn btn-primary" onClick={this.onSubmit}>
